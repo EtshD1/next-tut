3055714078fb018184e36f9ec04fb5d1c9bb5247
EtshD1 <mohamed.h.abouelenin@gmail.com>
2024-01-31 23:43:50 +0200

    data streaming

--- app/ui/dashboard/revenue-chart.tsx
+++ app/ui/dashboard/revenue-chart.tsx
@@ -8,14 +8,12 @@ import { Revenue } from '@/app/lib/definitions';
 // https://www.tremor.so/
 // https://www.chartjs.org/
 // https://airbnb.io/visx/
+import { fetchRevenue } from '@/app/lib/data';
 
-export default async function RevenueChart({
-  revenue,
-}: {
-  revenue: Revenue[];
-}) {
+export default async function RevenueChart() {
   const chartHeight = 350;
   // NOTE: comment in this code when you get to this point in the course
+  const revenue = await fetchRevenue();
 
   const { yAxisLabels, topLabel } = generateYAxis(revenue);
 
