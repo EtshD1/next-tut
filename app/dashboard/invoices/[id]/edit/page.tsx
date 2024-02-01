ac4b0fcbcf81480afc492bc65afa4a890926c42a
EtshD1 <mohamed.h.abouelenin@gmail.com>
2024-02-01 16:56:20 +0200

    Add metadata

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -1,7 +1,16 @@
-import { fetchInvoiceById, fetchCustomers } from '@/app/lib/data';
+import {
+  fetchInvoiceById,
+  fetchCustomers,
+  fetchCustomerById,
+} from '@/app/lib/data';
 import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
 import Form from '@/app/ui/invoices/edit-form';
+import { Metadata, ResolvingMetadata } from 'next';
 import { notFound } from 'next/navigation';
+
+interface Props {
+  params: { id: string };
+}
 
 const Page = async ({ params }: { params: { id: string } }) => {
   const id = params.id;
@@ -32,3 +41,24 @@ const Page = async ({ params }: { params: { id: string } }) => {
 };
 
 export default Page;
+
+export async function generateMetadata(
+  { params }: Props,
+  parent: ResolvingMetadata,
+): Promise<Metadata> {
+  // read route params
+  const id = params.id;
+  const invoice = await fetchInvoiceById(id);
+  const customer = invoice
+    ? await fetchCustomerById(invoice.customer_id)
+    : undefined;
+
+  if (customer)
+    return {
+      title: `Edit ${customer.name}'s Invoice`,
+    };
+
+  return {
+    title: 'Edit Invoice',
+  };
+}
